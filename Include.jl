240ba06e6d88cfa39f0a748e9af220af35facc8f
Jeffrey Varner <jdv27@cornell.edu>
2023-09-05 09:33:37 -0400

    Fruit problem Cobb Douglas
    CD fruit problem

--- Include.jl
+++ Include.jl
@@ -16,6 +16,15 @@ using Plots
 using Colors
 using Distributions
 
+# load my color palette -
+colors = Dict{Int,RGB}();
+colors[1] = colorant"#0077BB"
+colors[2] = colorant"#88CCEE"
+colors[3] = colorant"#EE7733"
+colors[4] = colorant"#CC3311"
+
 # load my codes -
 include(joinpath(_PATH_TO_SRC, "Compute.jl"));
 include(joinpath(_PATH_TO_SRC, "Files.jl"));
+include(joinpath(_PATH_TO_SRC, "Indifference.jl"));
+include(joinpath(_PATH_TO_SRC, "Budget.jl"));
