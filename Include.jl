25d935e541964673e28ecccde59b0c1bba70dcbb
Jeffrey Varner <jdv27@cornell.edu>
2023-09-21 09:54:50 -0400

    5760 Insurance problem
    insurance problem

--- Include.jl
+++ Include.jl
@@ -4,8 +4,8 @@ const _PATH_TO_SRC = joinpath(_ROOT, "src");
 const _PATH_TO_DATA = joinpath(_ROOT, "data");
 
 # download external packages
-using Pkg;
-Pkg.add(path="https://github.com/varnerlab/VLDecisionsPackage.jl.git")
+# using Pkg;
+# Pkg.add(path="https://github.com/varnerlab/VLDecisionsPackage.jl.git")
 # Pkg.activate("."); Pkg.instantiate(); Pkg.update()
 
 # load external packages -
